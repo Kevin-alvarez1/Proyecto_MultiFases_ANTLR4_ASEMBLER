3904069ed368d1fb78592c8db51b38e47d576bf7
Kevin-alvarez1 <3871081810101@ingenieria.usac.edu.gt>
2025-06-19 15:39:57 -0600

    Se agrego la traducion a asembler de print y println

--- VAsm/assembler/program.s
+++ VAsm/assembler/program.s
@@ -1,17 +1,16 @@
-.section .text
-    
-.globl _start
+.data
+msg_0: .ascii "223123\n"
+msg_1: .ascii "0\n"
 
-    mov x0, #1
-    adr x1, msg
-    mov x2, len
-    mov w8, #64
-    svc #0
+.text
+mov x0, #1
+adr x1, msg_0
+mov x2, #7
+mov w8, #64
+svc #0
 
-    mov x0, #0
-    mov w8, #93
-    svc #0
-
-
-msg: .ascii "Hello, World!\n"
-len = . - msg
+mov x0, #1
+adr x1, msg_1
+mov x2, #2
+mov w8, #64
+svc #0
